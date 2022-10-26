e9965becd3d08cee6dc92e7a14f3c8d7a7b1f23e
Văn Thế Vinh <ewsxztrfvcuyhnb@gmail.com>
2022-10-26 11:35:03 +0700

    adding  cors

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const userRouter = require('./Routes/user');
 const middlewares = require('./middlewares/middlewares');
-//const cors = require('cors');
+const cors = require('cors');
 const express = require('express');
 const app = express();
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRouter);
