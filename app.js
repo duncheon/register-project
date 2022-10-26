f0c36751469c3ebba017ee422fd46df47c2ca145
Văn Thế Vinh <ewsxztrfvcuyhnb@gmail.com>
2022-10-26 11:17:36 +0700

    remove static build

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/users', userRouter);
+
 app.use('/', (req, res) => {
   res.json('Welcome to VTV server');
 });
-app.use('/api/users', userRouter);
-
 app.use(middlewares.unknownEndpoint);
 
 module.exports = app;
